f500081cb21284fdb968584a44d35283ba577486
kenyk7 <kenykalin@gmail.com>
2025-09-23 11:27:06 -0500

    feat: mejoras de css, prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,9 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  cacheDir: './.astro',
   integrations: [react()],
+  vite: {
+    cacheDir: './.vite',
+  },
 });
---
+++ src/components/CardReact.tsx
@@ -0,0 +1,8 @@
+export function CardReact() {
+  return (
+    <div className="card-react">
+      <h2>Card Title React</h2>
+      <p>This is a description of the card.</p>
+    </div>
+  );
+}
